21f4343136f48a476d22445df9e4af48ee0fd815
zifrit <zifrit@list.ru>
2024-12-09 17:28:51 +0300

    added settings

---
+++ auth/src/core/settings.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AUTH_BASE_DIR = Path(__file__).parent.parent
+
+
+class DBSettings(BaseSettings):
+    AUTH_DB_HOST: str = "localhost"
+    AUTH_DB_PORT: int = 5432
+    AUTH_DB_USER: str = "postgres"
+    AUTH_DB_PASS: str = "postgres"
+    AUTH_DB_NAME: str = "postgres"
+
+    @property
+    def auth_async_database_url(self):
+        return f"postgresql+asyncpg://{self.AUTH_DB_USER}:{self.AUTH_DB_PASS}@{self.AUTH_DB_HOST}:{self.AUTH_DB_PORT}/{self.AUTH_DB_NAME}"
+
+
+class JWTSettings(BaseSettings):
+    AUTH_PRIVATE_KEY_FILE: str
+    AUTH_PUBLIC_KEY_FILE: str
+    CRYPTO_ALGORITHM: str = "RS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
+
+    @property
+    def jwt_private_key(self) -> Path:
+        return AUTH_BASE_DIR / "core/certs" / self.AUTH_PRIVATE_KEY_FILE
+
+    @property
+    def jwt_public_key(self) -> Path:
+        return AUTH_BASE_DIR / "core/certs" / self.AUTH_PUBLIC_KEY_FILE
+
+
+class Settings(BaseSettings):
+    db_settings: DBSettings = DBSettings()
+    jwt_settings: JWTSettings = JWTSettings()
+
+
+settings = Settings()
