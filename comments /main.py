f2345623bb7effbd6544fe3263c3a0355e07eb47
zifrit <zifrit@list.ru>
2024-12-13 17:28:27 +0300

    added runner file for comments and logging config for comments

---
+++ comments /main.py
@@ -0,0 +1,22 @@
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+from src.utils.logger import LOGGING
+from src.core.settings import settings
+
+app = FastAPI(
+    title=settings.PROJECT_TITLE,
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        port=8003,
+        reload=True,
+        log_level=logging.DEBUG,
+        log_config=LOGGING,
+    )
